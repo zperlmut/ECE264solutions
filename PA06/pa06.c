5e563b397b50921c207fa4a3bd41547137109638
Zachary Jacob Perlmutter <zperlmut@ecelinux20.ecn.purdue.edu>
2014-02-28 14:29:22 -0500

    rev A

---
+++ PA06/pa06.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "answer06.h"
+
+int main(int argc, char **argv)
+{
+  if(argc != 2) return EXIT_FAILURE;
+  int value = atoi(argv[1]);
+  //partitionAll(value);
+  //partitionIncreasing(value);
+  //partitionDecreasing(value);
+  /* partitionOdd(value);
+  printf("\n\n\n");
+  partitionEven(value);
+  printf("\n\n\n");
+  */
+  partitionPrime(value);  
+  printf("\n\n\n");
+
+  return EXIT_SUCCESS;
+
+
+}
