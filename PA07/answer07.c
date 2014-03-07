deb1a09fbe2c80bdce741abb5f792dffc0df241f
Zachary Jacob Perlmutter <zperlmut@ecelinux17.ecn.purdue.edu>
2014-03-07 12:55:24 -0500

    REV B

--- PA07/answer07.c
+++ PA07/answer07.c
@@ -24,3 +24,65 @@ int List_length(List *list)
   if (list == NULL) return 0;
   return 1 + List_length(list->next);
 }
+
+List *List_merge(List * lhs, List * rhs, int (*compar)(const char *, const char*))
+{
+  if (lhs == NULL) return rhs;
+  if (rhs == NULL) return lhs;
+
+  List *head = NULL;
+  List *tail = NULL;
+
+  while (lhs != NULL && rhs != NULL){
+    int cmp = compar(lhs->str, rhs->str);
+    if (cmp <= 0){
+      if (tail == NULL){
+	head = lhs;
+	tail = lhs;
+      }
+      else {
+	tail->next = lhs;
+	tail = lhs;
+      }
+    }
+    else {
+      if(tail == NULL){
+	head = rhs;
+	tail = rhs;
+      }
+      else {
+	tail->next = rhs;
+	tail = rhs;
+      }
+      rhs = rhs->next;
+      tail->next = NULL;
+    }
+  }
+}
+
+List * List_sort(List * list, int (*compar)(const char *, const char*))
+{
+  //Base Case
+  int len = List_length(list);
+  if (len <= 1) return list;
+
+  //Recursive Case
+
+  //Split Phase
+  int half_len = len / 2;
+  List *lhs = list;
+  List *lhs_tail = list;
+
+  while (--half_len > 0){
+    lhs_tail = lhs_tail->next;
+  }
+  List *rhs = lhs_tail->next;
+  lhs_tail->next = NULL;
+
+  //Merge Phase
+  lhs = List_sort(lhs, compar);
+  rhs = List_sort(rhs, compar);
+  
+return List_merge(lhs, rhs, compar);
+  
+}
